55bc574652f0357c6955074722db5977655e2d62
thaiph99 <thaiph99@gmail.com>
2020-10-29 10:08:15 +0700

    modified ST 1

--- segment tree/temp.cpp
+++ segment tree/temp.cpp
@@ -9,8 +9,43 @@ using namespace std;
     cin.tie(0);                   \
     cout.tie(0);
 
+const ll nax = 1e7;
+ll ST[nax];
+void update(int id, int l, int r, int i, int v)
+{
+    if (i < l || r < i)
+    {
+        return;
+    }
+    if (l == r)
+    {
+        ST[id] = v;
+        return;
+    }
+
+    // Gọi đệ quy để xử lý các nút con của nút id
+    int mid = (l + r) / 2;
+    update(id * 2, l, mid, i, v);
+    update(id * 2 + 1, mid + 1, r, i, v);
+    ST[id] = max(ST[id * 2], ST[id * 2 + 1]);
+}
+
+int get(int id, int l, int r, int u, int v)
+{
+    if (v < l || r < u)
+    {
+        return -INFINITY;
+    }
+    if (u <= l && r <= v)
+    {
+        return ST[id];
+    }
+    int mid = (l + r) / 2;
+    return max(get(id * 2, l, mid, u, v), get(id * 2 + 1, mid + 1, r, u, v));
+}
+
 int main(int argc, char const *argv[])
 {
-    
+
     return 0;
 }
